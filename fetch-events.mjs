717d8adc44c108dde5a7aecaade187fa37872a87
Eric Wehrly <eric.wehrly@florencehc.com>
2023-09-14 13:53:48 -0400

    mailgun event time range as variable

--- fetch-events.mjs
+++ fetch-events.mjs
@@ -9,12 +9,12 @@ const API_KEY = process.env.MAILGUN_KEY;
 const mailgun = new Mailgun(formData);
 const client = mailgun.client({ username: 'api', key: API_KEY });
 
-export default async function fetchEvents() {
+export default async function fetchEvents(options) {
 
     try {
         // const date = new Date(2021, 10, 1, 0, 0, 0, 0); // Mon Nov 01 2021 00:00:00 GMT+0200
         const date = new Date();
-        date.setDate(date.getDate() - 2);
+        date.setDate(date.getDate() - options.daysPast || 0);
   
         const events = await client.events.get(DOMAIN, {
           begin: date.toGMTString(), // Sun, 31 Oct 2021 22:00:00 GMT
--- index.js
+++ index.js
@@ -2,5 +2,7 @@ import fetchEvents from './fetch-events.mjs';
 
 console.log("Hello world.");
 
-const { pages, items: events } = await fetchEvents();
+const { pages, items: events } = await fetchEvents({
+    daysPast: 2
+});
 console.log(events.length);

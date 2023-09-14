ca1d42aa1f5aa025f5d1365f494d454c4f1a8299
Eric Wehrly <eric.wehrly@florencehc.com>
2023-09-14 15:24:29 -0400

    now we loop through events

--- fetch-events.mjs
+++ fetch-events.mjs
@@ -6,23 +6,34 @@ dotenv.config();
 const DOMAIN = 'researchbinders.com';
 const API_KEY = process.env.MAILGUN_KEY;
 
+const DEFAULT_EVENT_LIMIT = 300;    // this also happens to be the max :)
+
 const mailgun = new Mailgun(formData);
 const client = mailgun.client({ username: 'api', key: API_KEY });
 
-export default async function fetchEvents(options) {
+// detailed event data not retained longer than 30 days
+export default async function fetchPagedEvents(options) {
 
     try {
         // const date = new Date(2021, 10, 1, 0, 0, 0, 0); // Mon Nov 01 2021 00:00:00 GMT+0200
         const date = new Date();
         date.setDate(date.getDate() - options.daysPast || 0);
   
-        const events = await client.events.get(DOMAIN, {
-          begin: date.toGMTString(), // Sun, 31 Oct 2021 22:00:00 GMT
-          ascending: 'yes',
-          event: 'failed',
-          limit: 5
-        });
-
+        const mailgunOptions = {
+            begin: date.toGMTString(), // Sun, 31 Oct 2021 22:00:00 GMT
+            // ascending: 'yes',
+            event: 'failed',
+            limit: options.limit || DEFAULT_EVENT_LIMIT
+          };
+          let events;
+          if(options.page) {
+            events = await client.events.get(DOMAIN, options);
+            console.log(`${events.items.length} events on next page.`);
+          } else {
+            events = await client.events.get(DOMAIN, mailgunOptions);
+            console.log(`Fetched ${events.items.length} events for ${date}.`);
+          }
+        
         return events;
 
     } catch (error) {
@@ -31,3 +42,31 @@ export default async function fetchEvents(options) {
 
     return null;
 };
+
+export async function fetchAllEvents(options) {
+
+    console.log('');
+    console.log(`Fetching all events ...`);
+
+    let iterationCount = 1;
+    const allEvents = [];
+    let events = await fetchPagedEvents(options);
+    allEvents.push(...events.items);
+    while(events?.pages?.next) {
+
+        // stop if we happened to have run out of events
+        if(events.items.length === 0) break;
+
+        iterationCount++;
+        events = await fetchPagedEvents({
+            page: events.pages.next.page
+        });
+        allEvents.push(...events.items);
+
+        // stop if there's (hopefully) no more events to grab
+        if(events.items.length < options.limit || DEFAULT_EVENT_LIMIT) break;
+    }
+
+    console.log(`${iterationCount} iterations retrieved ${allEvents.length} total events.`);
+    return allEvents;
+}
--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-import fetchEvents from './fetch-events.mjs';
+import { fetchAllEvents } from './fetch-events.mjs';
 
 console.log("App starting.");
 
-// TODO: loop through pages and gather all events
-const { pages, items: events } = await fetchEvents({
-    daysPast: 2
-});
-
-const domains = {};
-events.forEach(event => {
+function getDomain(event) {
 
     let recipientEmails = event.envelope.targets;
     if(!Array.isArray(recipientEmails)) {
         recipientEmails = [recipientEmails];
     }
+
     recipientEmails.forEach(email => {
 
         const domain = email.split('@')[1];
-        const message = event['delivery-status']?.description 
-            || event['delivery-status']?.message 
-            || '';
-        if(!domains[domain]) {
-            domains[domain] = {
-                reasons: []
-            };
-        } 
-        if(!domains[domain].reasons.includes(message)) {
-            domains[domain].reasons.push(message);
-        }
+        return domain;
     });
+
+}
+
+const events = await fetchAllEvents({
+    daysPast: 1
 });
 
+const domains = {};
+events.forEach(event => {
+
+    const domain = event['recipient-domain'];
+
+    if(!domain.length) {
+        console.warn(`No domain for event.`);
+        return;
+    }
+
+    const message = event['delivery-status']?.description 
+        || event['delivery-status']?.message 
+        || '';
+    if(!domains[domain]) {
+        domains[domain] = {
+            reasons: []
+        };
+    } 
+    if(!domains[domain].reasons.includes(message)) {
+        domains[domain].reasons.push(message);
+    }
+});
+
+console.log('');
 console.log(`Domains:`, domains);

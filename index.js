417a47c71107f02a567ed2ae88f69e73cc5e7199
Eric Wehrly <eric.wehrly@florencehc.com>
2023-09-14 14:43:33 -0400

    extract email domains and corresponding messages from events

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 import fetchEvents from './fetch-events.mjs';
 
-console.log("Hello world.");
+console.log("App starting.");
 
+// TODO: loop through pages and gather all events
 const { pages, items: events } = await fetchEvents({
     daysPast: 2
 });
-console.log(events.length);
+
+const domains = {};
+events.forEach(event => {
+
+    let recipientEmails = event.envelope.targets;
+    if(!Array.isArray(recipientEmails)) {
+        recipientEmails = [recipientEmails];
+    }
+    recipientEmails.forEach(email => {
+
+        const domain = email.split('@')[1];
+        const message = event['delivery-status']?.description 
+            || event['delivery-status']?.message 
+            || '';
+        if(!domains[domain]) {
+            domains[domain] = {
+                reasons: []
+            };
+        } 
+        if(!domains[domain].reasons.includes(message)) {
+            domains[domain].reasons.push(message);
+        }
+    });
+});
+
+console.log(`Domains:`, domains);

533d4b8715e7e27691b96f52600fcde0472be586
Eric Wehrly <eric.wehrly@florencehc.com>
2023-09-14 16:11:57 -0400

    output events to file so that they can be re-parsed without needing to re-fetch

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ import fs from 'fs';
 
 console.log("App starting.");
 
+// TODO: if events file exists, allow to read from that instead
 const events = await fetchAllEvents({
     daysPast: 29
 });
 
 const domains = getDomainReasons(events);
 
-fs.writeFileSync('./domains.json', JSON.stringify(domains, null, 2));
+try {
+    if(!fs.existsSync('./output')){
+        fs.mkdirSync('./output');
+    }
+    fs.writeFileSync('./output/events.json', JSON.stringify(events, null, 2));
+    fs.writeFileSync('./output/domains.json', JSON.stringify(domains, null, 2));
+} catch (ex) {
+    console.error("Error writing files.");
+    console.error(ex);
+}

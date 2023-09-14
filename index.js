92b2baa1718a049674e0b3a4a5cbbf56b4157309
Eric Wehrly <eric.wehrly@florencehc.com>
2023-09-14 13:18:16 -0400

    use the correct mailgun library; fix loading .env files; get queries working!

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-const DOMAIN = 'researchbinders.com';
-
 import formData from 'form-data';
-import { Mailgun } from "mailgun";
+import Mailgun from 'mailgun.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const DOMAIN = 'researchbinders.com';
+const API_KEY = process.env.MAILGUN_KEY;
+console.log(API_KEY);
 
 const mailgun = new Mailgun(formData);
-// const client = mailgun.client({ username: 'api', key: 'YOUR_API_KEY' || '' });
+const client = mailgun.client({ username: 'api', key: API_KEY });
 
-console.log("Hello world.");
+console.log("Client established, probably.");
+
+(async () => {
+    try {
+      // const date = new Date(2021, 10, 1, 0, 0, 0, 0); // Mon Nov 01 2021 00:00:00 GMT+0200
+      const date = new Date();
+      date.setDate(date.getDate() - 2);
+
+      const events = await client.events.get(DOMAIN, {
+        begin: date.toGMTString(), // Sun, 31 Oct 2021 22:00:00 GMT
+        ascending: 'yes',
+        limit: 5
+      });
+      console.log('events', events)
+    } catch (error) {
+        console.error(error);
+    }
+  })();
